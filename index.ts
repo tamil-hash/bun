5c45ad739ba32d0e3bd1fa886051711ab0359239
Tamil Selvan S <tamil.selvans@raweng.com>
2025-03-27 21:22:47 +0530

    bun basics

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Executed from bunnnnnn")
+import figlet from "figlet";
+
+const server = Bun.serve({
+  port: 3000,
+  fetch(req) {
+    console.log(req)
+    const body = figlet.textSync("Tamil!")
+    console.log(Bun.version);
+    console.log("gg")
+    return new Response(body);
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
